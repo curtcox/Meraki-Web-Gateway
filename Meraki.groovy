1bc4dde34991a29ac340b1e218b387bbe48e070d
Curt Cox <curt.cox@asynchrony.com>
2016-12-02 10:06:04 -0600

    Add Meraki URL being requested to error page.

--- Meraki.groovy
+++ Meraki.groovy
@@ -3,13 +3,15 @@ import groovy.json.*
 class Meraki {
 
   def get(request) {
-      def command = "./get.sh https://n124.meraki.com/api/v0/$request"
+      def page    = "https://n124.meraki.com/api/v0/$request"
+      def command = "./get.sh $page"
       System.err.println(command)
       def text = command.execute().text
       try {
-        return new JsonSlurper().parseText(text)
+          return new JsonSlurper().parseText(text)
       } catch (e) {
-         throw new RuntimeException(text,e)
+          def errorPage = text.replaceAll("page","page ($page)")
+          throw new RuntimeException(errorPage,e)
       }
   }
 
