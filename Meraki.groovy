f597f86bc4a63ee9321df40e78ecf7faaf7ccb88
Curt Cox <curt.cox@asynchrony.com>
2016-12-02 09:20:09 -0600

    Relay Meraki error pages directly to the browser

--- Meraki.groovy
+++ Meraki.groovy
@@ -5,7 +5,12 @@ class Meraki {
   def get(request) {
       def command = "./get.sh https://n124.meraki.com/api/v0/$request"
       System.err.println(command)
-      return new JsonSlurper().parseText(command.execute().text)
+      def text = command.execute().text
+      try {
+        return new JsonSlurper().parseText(text)
+      } catch (e) {
+         throw new RuntimeException(text,e)
+      }
   }
 
 }
--- Router.groovy
+++ Router.groovy
@@ -2,7 +2,12 @@ def port() { return 8080 }
 
 def returnResponseFromMeraki() {
     response.contentType = 'application/json'
-    println new MerakiBrowser(port(),request).response()
+    try {
+      println new MerakiBrowser(port(),request).response()
+    } catch (e) {
+      response.contentType = 'text/html'
+      println e.message
+    }
 }
 
 def root() {
