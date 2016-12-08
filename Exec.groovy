31b4d130fe730c88b6530680b34cf4e6cb6fc38d
Curt Cox <curt.cox@asynchrony.com>
2016-12-08 11:53:18 -0600

    Whitespace

--- Exec.groovy
+++ Exec.groovy
@@ -1,15 +1,15 @@
 class Exec {
 
-  static def prompt(request) {
-      def html = new File('prompt.html').text
-      def command = request.getParameter("cmd")
-      if (command) {
-          html = html.replaceAll('command',command.replaceAll('"','&quot;'))
-          def result = exec(command)
-          html = html.replaceAll('result',result)
-      }
-      return html
-  }
+    static def prompt(request) {
+        def html = new File('prompt.html').text
+        def command = request.getParameter("cmd")
+        if (command) {
+            html = html.replaceAll('command', command.replaceAll('"', '&quot;'))
+            def result = exec(command)
+            html = html.replaceAll('result', result)
+        }
+        return html
+    }
 
     static def exec(command) {
         System.err.println("command=" + command)
@@ -18,7 +18,7 @@ class Exec {
         def text = result.text
         System.err.println("text=" + text)
         return "command=$command\r" +
-               "result=$result\r" +
-               "text=$text\r"
+                "result=$result\r" +
+                "text=$text\r"
     }
 }
